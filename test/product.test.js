a1e81d319ba119c246a443ecb5d803da3de46bf4
Abdul Ahad <abdulahad@IS-KHI-117.local>
2024-01-01 17:53:03 +0500

    mongo connection in unit test

--- test/product.test.js
+++ test/product.test.js
@@ -4,6 +4,15 @@ const app = require("../app");
 
 require("dotenv").config();
 
+/* Connecting to the database before each test. */
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+  });
+  
+  /* Closing database connection after each test. */
+  afterEach(async () => {
+    await mongoose.connection.close();
+  });
 
   describe("GET /api/products/:id", () => {
     it("should return a product", async () => {
